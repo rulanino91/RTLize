5b8eda5fd8c73d220e3166a25785fbbe4acae099
Fady Naffa <fadynaffa3@gmail.com>
2019-02-06 13:10:48 +0200

    ADD RAILS 4 support

--- lib/rtlize/railtie.rb
+++ lib/rtlize/railtie.rb
@@ -8,7 +8,7 @@ module Rtlize
     config.rtlize.rtl_locales  = Rtlize.rtl_locales
 
     initializer "rtlize.railtie", :after => "sprockets.environment" do |app|
-      Sprockets.register_postprocessor 'text/css', Rtlize::RtlProcessor
+      app.assets.register_postprocessor 'text/css', Rtlize::RtlProcessor
 
       Rtlize.rtl_selector = config.rtlize.rtl_selector
       Rtlize.rtl_locales  = config.rtlize.rtl_locales
