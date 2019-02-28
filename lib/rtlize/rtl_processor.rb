caf04bb2ae3b3aec1d5a66fb1af83df45e7d5fea
Fady Naffa <fadynaffa3@gmail.com>
2019-02-28 10:41:50 +0200

    rails 5 deprecation notes

--- lib/rtlize/rtl_processor.rb
+++ lib/rtlize/rtl_processor.rb
@@ -1,19 +1,34 @@
 module Rtlize
   class RtlProcessor
-    attr_reader :data
+    require 'sprockets/processing'
+    extend Sprockets::Processing
 
-    def initialize(file, &block)
-      @data = block.call
+    def initialize(filename, &block)
+      @filename = filename
+      @source   = block.call
     end
 
-    def render(context, locals, &block)
+    def render(context, _options)
+      self.class.run(@filename, @source, context)
+    end
+
+    def self.run(filename, source, context)
       allowed_extensions = ['sass', 'css', 'scss']
       extension = context.pathname.basename.to_s.split('.').length > 1 ? context.pathname.basename.to_s.split('.')[-1] : nil
       if extension && allowed_extensions.include?(extension) && context.pathname.basename.to_s.match(/\.rtl/i)
-        Rtlize::RTLizer.transform(data)
+        Rtlize::RTLizer.transform(source)
       else
-        data
+        source
       end
+    end
+
+    def self.call(input)
+      filename = input[:filename]
+      source   = input[:data]
+      context  = input[:environment].context_class.new(input)
+
+      result = run(filename, source, context)
+      context.metadata.merge(data: result)
     end
   end
 end
